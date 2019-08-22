d197ec3e717412371f08d0f81870a2ad22eeacf6
xsenia <belkinasan@yandex.ru>
2019-08-22 22:11:58 +0500

    add console log

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let money, income, addExpenses, deposit, mission, period;
 
+
 money = 0;
 period = 'Период';
 
 alert('Деньги - ' + money);
 console.log('period: ', period);
+console.log('addExpenses: ', addExpenses);
